b7787f5a2faa876b42eb50542c187c123a047ca8
Peter Lin <peterlin741@gmail.com>
2016-02-10 04:16:59 -0500

    updating env

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,13 @@ module InterviewConnect
 
     config.time_zone = 'Eastern Time (US & Canada)'
     # config.active_record.default_timezone = 'Eastern Time (US & Canada)'
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+    
   end
 end
